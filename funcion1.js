e153002cd6511b3bdf477c67f6da84de5809d6df
diegomerlo <diego.e.merlo@gmail.com>
2024-06-28 16:34:23 -0300

    update

---
+++ funcion1.js
@@ -0,0 +1,9 @@
+let objeto_demo = {
+    uno: 1, dos:2, tres: 3
+}
+
+let uno = "tres";
+
+console.log(objeto_demo['uno']); //devuelve el numero
+
+console.log(" -"+objeto_demo[uno]) // Devuelve el numero 3
---
+++ funcion2.js
@@ -0,0 +1,14 @@
+let capitales = {
+    argentina: "Buenos Aires",
+    uruguay: "Montevideo"
+}
+
+let lugar = "uruguay"
+
+console.log(capitales['lugar']);//undefine
+console.log(capitales.lugar); //undefine
+console.log(capitales[lugar]);//Montevideo
+// El primero pasa el nombre de la variable subyacente que va a buscar, en este caso lugar, 
+//como no existe dentri de capitales, queda referenciada como undefine
+// capitales.lugar no existe, si se quisiese se podría capitales.argentina
+
