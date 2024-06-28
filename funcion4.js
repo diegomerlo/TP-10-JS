025a3cffb7e9540076b2d4b926ffeccdca35c287
diegomerlo <diego.e.merlo@gmail.com>
2024-06-28 17:24:20 -0300

    update

--- funcion4.js
+++ funcion4.js
@@ -10,4 +10,14 @@ let propertyKey = "modelo";
 
 miAuto[propertyKey] = "Corolla";
 
-console.log(miAuto);
+let anotherPropertyKey = "precio";
+
+miAuto[anotherPropertyKey] = 25000;  // Por ejemplo, 25000
+
+let nextProperty = "color";
+
+miAuto[nextProperty] = "Rojo";
+
+for (let key in miAuto) {//for each
+    console.log(key + ": " + miAuto[key]);
+}
